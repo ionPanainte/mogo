cc622788e7b865214a029f19b0fad6e72d2462e2
Ion MORARU <ion.moraru.c@thalesdigital.io>
2018-04-17 21:59:08 +0200

    add produts

--- scripts/showModal.js
+++ scripts/showModal.js
@@ -1,7 +1,5 @@
 function myFunction(id) {
     var div = document.getElementById(id);
-    console.log(div)
-    console.log(id)
 
     if (div.style.display === "") {
         div.style.display = "block";
